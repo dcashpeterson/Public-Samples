dfd854dc086a81f4c1a561d922e145577406bace
Derek <derekcp@sympraxisconsulting.com>
2020-02-22 11:28:44 -0800

    Hello World For Teams

---
+++ Code/SPFX-For-Teams/HelloWorldForTeams/src/webparts/helloWorldForTeams/HelloWorldForTeamsWebPart.ts
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+
+import * as strings from 'HelloWorldForTeamsWebPartStrings';
+import HelloWorldForTeams from './components/HelloWorldForTeams';
+import { IHelloWorldForTeamsProps } from './components/IHelloWorldForTeamsProps';
+
+export interface IHelloWorldForTeamsWebPartProps {
+  description: string;
+}
+
+export default class HelloWorldForTeamsWebPart extends BaseClientSideWebPart<IHelloWorldForTeamsWebPartProps> {
+
+  public render(): void {
+
+    const element: React.ReactElement<IHelloWorldForTeamsProps> = React.createElement(
+      HelloWorldForTeams,
+      {
+        description: this.properties.description,
+        context: this.context
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ Code/SPFX-For-Teams/HelloWorldForTeams/src/webparts/helloWorldForTeams/components/IHelloWorldForTeamsProps.ts
@@ -0,0 +1,6 @@
+import { WebPartContext } from "@microsoft/sp-webpart-base";
+
+export interface IHelloWorldForTeamsProps {
+  description: string;
+  context: WebPartContext;
+}
