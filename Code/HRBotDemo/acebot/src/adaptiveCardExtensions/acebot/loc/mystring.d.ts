deb70c00acde4e46a0cfae31a5c1cd6613181418
Derek <derekcp@sympraxisconsulting.com>
2024-04-30 10:50:40 -0400

    update changes

---
+++ Code/HRBotDemo/acebot/src/adaptiveCardExtensions/acebot/loc/mystring.d.ts
@@ -0,0 +1,14 @@
+declare interface IAcebotAdaptiveCardExtensionStrings {
+  PropertyPaneDescription: string;
+  TitleFieldLabel: string;
+  Title: string;
+  SubTitle: string;
+  PrimaryText: string;
+  Description: string;
+  QuickViewButton: string;
+}
+
+declare module 'AcebotAdaptiveCardExtensionStrings' {
+  const strings: IAcebotAdaptiveCardExtensionStrings;
+  export = strings;
+}
