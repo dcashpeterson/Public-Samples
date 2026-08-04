bd1dcdef7859775e507827d9c0bd9f7b224b5d9a
Derek Cash-Peterson <derekcp@sympraxisconsulting.com>
2026-08-04 12:02:37 +0000

    update to list service

--- Code/LPAnalytics/src/services/listService.ts
+++ Code/LPAnalytics/src/services/listService.ts
@@ -1,26 +1,6 @@
-import "@pnp/graph/content-types/index.js";
-import "@pnp/graph/files/index.js";
-import "@pnp/graph/groups/index.js";
-import "@pnp/graph/index.js";
-import "@pnp/graph/lists/drive.js";
-import "@pnp/graph/lists/index.js";
-import "@pnp/graph/members/index.js";
-import "@pnp/graph/sites/index.js";
-import "@pnp/graph/teams/index.js";
-import "@pnp/graph/users/index.js";
-import "@pnp/graph/workbooks/index.js";
-import "@pnp/sp-admin/index.js";
-import "@pnp/sp/attachments/index.js";
-import "@pnp/sp/batching.js";
-import "@pnp/sp/clientside-pages/index.js";
-import "@pnp/sp/files/index.js";
-import "@pnp/sp/folders/index.js";
-import "@pnp/sp/hubsites/index.js";
 import "@pnp/sp/items/index.js";
 import "@pnp/sp/lists/index.js";
 import "@pnp/sp/sites/index.js";
-import "@pnp/sp/subscriptions/index.js";
-import "@pnp/sp/views/index.js";
 import "@pnp/sp/webs/index.js";
 import { AssignFrom } from "@pnp/core/index.js";
 import { spfi } from "@pnp/sp/index.js";
@@ -46,9 +26,7 @@ export class ListService implements IListService {
   public async Process(queueItem: ILPAnalyticsEvent): Promise<boolean> {
     try {
       var retVal = false;
-      const spSite = spfi("https://sympdcp.sharepoint.com/sites/LearningPathwaysAnalytics").using(AssignFrom(this._auth.sp.web));
-      var _web = spSite.web;
-      const analyticsSite = spfi("https://sympdcp.sharepoint.com/sites/LearningPathwaysAnalytics").using(AssignFrom(this._auth.sp.web));
+      const analyticsSite = spfi(process.env.AnalyticsSite).using(AssignFrom(this._auth.sp.web));
       const analyticsWeb = analyticsSite.web;
       await analyticsWeb.lists.getByTitle(process.env.ListName).items.add({
         Title: queueItem.user + new Date().toISOString(),
